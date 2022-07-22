bed1f059d9f2368108b07605a777122a199056c2
Pablo Henrique <pablohenrique.r.c@gmail.com>
2022-07-22 00:37:54 -0400

    Implementa tela de registro de solicitação

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ import { Center, Spinner } from 'native-base';
 
 export const Loading = () => {
   return (
-    <Center flex={1} bg="gray.600">
+    <Center flex={1} bg="gray.700">
       <Spinner color="secondary.700" />
     </Center>
   );
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
+import firestore from '@react-native-firebase/firestore';
+import { useNavigation } from '@react-navigation/native';
 import { VStack } from 'native-base';
 
 import { Button } from '../components/Button';
@@ -7,18 +10,66 @@ import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 
 export const Register = () => {
+  const navigation = useNavigation();
+  const [isLoading, setLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleNewOrderRegister = () => {
+    if (!patrimony || !description) {
+      return Alert.alert('Registrar', 'Preencha todos os campos.');
+    }
+
+    setLoading(true);
+
+    firestore()
+      .collection('orders')
+      .add({
+        patrimony,
+        description,
+        status: 'open',
+        created_at: firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        Alert.alert('Registro', 'Solicitação registrada com sucesso!');
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        return Alert.alert(
+          'Registro',
+          'Não foi possível registrar a solicitação.'
+        );
+      });
+  };
+
   return (
-    <VStack flex={1} p={6} bg="gray.600">
-      <Header title="Nova solicitação" />
-      <Input placeholder="Número do patrimônio" mt={4} />
-      <Input
-        flex={1}
-        mt={5}
-        multiline
-        textAlignVertical="top"
-        placeholder="Descrição do problema"
-      />
-      <Button title="Cadastrar" mt="5" />
+    <VStack flex={1} bg="gray.600">
+      <Header title="Solicitação" />
+      <VStack flex={1} px={6} pb={6}>
+        <Input
+          placeholder="Número do patrimônio"
+          mt={4}
+          value={patrimony}
+          onChangeText={setPatrimony}
+        />
+        <Input
+          flex={1}
+          mt={5}
+          multiline
+          value={description}
+          onChangeText={setDescription}
+          textAlignVertical="top"
+          placeholder="Descrição do problema"
+        />
+        <Button
+          title="Cadastrar"
+          isLoading={isLoading}
+          onPress={handleNewOrderRegister}
+          mt="5"
+        />
+      </VStack>
     </VStack>
   );
 };
